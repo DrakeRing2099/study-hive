08623f81897385c283e78bd1c13dcc4ae9afdd2d
DrakeRing2099 <sarthakpathak567@gmail.com>
2024-09-28 12:50:21 +0530

    Added basic uploading functionality with checks

--- studyhive/library/views.py
+++ studyhive/library/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render, redirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User 
 from django import forms
+from .models import Resource, Tag, Subject
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
@@ -24,6 +25,51 @@ class RegistrationForm(forms.ModelForm):
         return cleaned_data
 
 
+class ResourceForm(forms.ModelForm):
+    class Meta:
+        model = Resource
+        fields = [
+            'title',
+            'description',
+            'resource_type',
+            'file_type',
+            'file',
+            'video_url',
+            'subject',
+            'tags',
+        ]
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
+            'file': forms.FileInput(),
+            'video_url': forms.URLInput(),
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ResourceForm, self).__init__(*args, **kwargs)
+        # Customize the fields as needed
+        self.fields['tags'].queryset = Tag.objects.all()
+        self.fields['subject'].queryset = Subject.objects.all()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        file = cleaned_data.get('file')
+        video_url = cleaned_data.get('video_url')
+        resource_type = cleaned_data.get('resource_type')
+
+        # Ensure that either file or video_url is provided, but not both
+        if not file and not video_url:
+            raise forms.ValidationError('Either file or video URL must be provided.')
+        if file and video_url:
+            raise forms.ValidationError('Only one of file or video URL should be provided.')
+
+        # Ensure resource_type matches the provided file or video_url
+        if file and resource_type != 'Document':
+            raise forms.ValidationError('Resource type must be "Document" when a file is provided.')
+        if video_url and resource_type != 'Video':
+            raise forms.ValidationError('Resource type must be "Video" when a video URL is provided.')
+
+        return cleaned_data
 
 
 def index(request):
@@ -74,3 +120,23 @@ def register(request):
     else:
         form = RegistrationForm()
     return render(request, "library/register.html", {"form": form})
+
+
+
+@login_required
+def upload_resource(request):
+    if request.method == 'POST':
+        form = ResourceForm(request.POST, request.FILES)
+        if form.is_valid():
+            resource = form.save(commit=False)
+            resource.uploader = request.user
+            resource.save()
+            form.save_m2m()  # Save the many-to-many data for tags
+            return redirect('resource_detail', resource_id=resource.id)
+        else:
+            # Form is invalid; render the form with errors
+            return render(request, 'library/upload_resource.html', {'form': form})
+    else:
+        form = ResourceForm()
+    return render(request, 'library/upload_resource.html', {'form': form})
+
