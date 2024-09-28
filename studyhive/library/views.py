8662987725ffbf022ac38808b7a03efdb4658e9a
DrakeRing2099 <sarthakpathak567@gmail.com>
2024-09-28 11:48:45 +0530

    Added login, logout, register functionality
    Also, profile dropdown is not working because of bootstrap, so I have adjusted for now

--- studyhive/library/views.py
+++ studyhive/library/views.py
@@ -1,6 +1,76 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User 
+from django import forms
 
-# Create your views here.
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirmation = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirmation = cleaned_data.get("confirmation")
+        if password != confirmation:
+            raise forms.ValidationError("Passwords do not match.")
+        return cleaned_data
+
+
+
+
 def index(request):
-    return render(request, "library/index.html")    
 
+    # Authenticated users view their inbox
+    if request.user.is_authenticated:
+        return render(request, "library/index.html")
+
+    # Everyone else is prompted to sign in
+    else:
+        return HttpResponseRedirect(reverse("login"))  
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "library/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "library/login.html")
+    
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            user = User.objects.create_user(username=username, email=email, password=password)
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "library/register.html", {"form": form})
+    else:
+        form = RegistrationForm()
+    return render(request, "library/register.html", {"form": form})
